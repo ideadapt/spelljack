06068a98b7189fafa46fdc5e7830b4c82d8c56e2
Ueli Kunz <kunz@ideadapt.net>
2022-09-28 16:59:13 +0200

    test new ref structure with context

--- test/test.mjs
+++ test/test.mjs
@@ -7,7 +7,7 @@ describe('merge findings', () => {
     describe('no existing', () => {
         it('should return new findings', () => {
             const existingFindings = []
-            const newFindings = [{ word: '', refs: [''] }]
+            const newFindings = [{ word: '', refs: [{ url: '' }] }]
 
             const merged = mergeFindings(newFindings, existingFindings)
 
@@ -17,7 +17,7 @@ describe('merge findings', () => {
 
     describe('no new', () => {
         it('should return existing', () => {
-            const existingFindings = [{ word: '', refs: [''] }]
+            const existingFindings = [{ word: '', refs: [{ url: '' }] }]
             const newFindings = []
 
             const merged = mergeFindings(newFindings, existingFindings)
@@ -29,57 +29,57 @@ describe('merge findings', () => {
     describe('new', () => {
         describe('without matching existing word', () => {
             it('should return existing plus new', () => {
-                const existingFindings = [{ word: 'a', refs: ['1'] }]
-                const newFindings = [{ word: 'b', refs: ['2'] }]
+                const existingFindings = [{ word: 'a', refs: [{ url: '1' }] }]
+                const newFindings = [{ word: 'b', refs: [{ url: '2' }] }]
 
                 const merged = mergeFindings(newFindings, existingFindings)
 
-                assert.deepEqual(merged, [{ word: 'a', refs: ['1'] }, { word: 'b', refs: ['2'] }])
+                assert.deepEqual(merged, [{ word: 'a', refs: [{ url: '1' }] }, { word: 'b', refs: [{ url: '2' }] }])
             })
         })
         describe('for existing word', () => {
             describe('different ref', () => {
                 it('should return merge by word', () => {
-                    const existingFindings = [{ word: 'b', refs: ['1'] }]
-                    const newFindings = [{ word: 'b', refs: ['2'] }]
+                    const existingFindings = [{ word: 'b', refs: [{ url: '1' }] }]
+                    const newFindings = [{ word: 'b', refs: [{ url: '2' }] }]
 
                     const merged = mergeFindings(newFindings, existingFindings)
 
-                    assert.deepEqual(merged, [{ word: 'b', refs: ['1', '2'] }])
+                    assert.deepEqual(merged, [{ word: 'b', refs: [{ url: '1' }, { url: '2' }] }])
                 })
             })
             describe('same ref', () => {
                 it('should return merge by word, no duplicate ref', () => {
-                    const existingFindings = [{ word: 'b', refs: ['1'] }]
-                    const newFindings = [{ word: 'b', refs: ['1'] }]
+                    const existingFindings = [{ word: 'b', refs: [{ url: '1' }] }]
+                    const newFindings = [{ word: 'b', refs: [{ url: '1', context: 'abc' }] }]
 
                     const merged = mergeFindings(newFindings, existingFindings)
 
-                    assert.deepEqual(merged, [{ word: 'b', refs: ['1'] }])
+                    assert.deepEqual(merged, [{ word: 'b', refs: [{ context: 'abc', url: '1' }] }])
                 })
             })
         })
         describe('for existing with multiple refs', () => {
             describe('different word', () => {
                 it('should return existing (with removed unfound ref) plus new', () => {
-                    const existingFindings = [{ word: 'aa', refs: ['1', '2'] }]
-                    const newFindings = [{ word: 'a', refs: ['1'] }]
+                    const existingFindings = [{ word: 'aa', refs: [{ url: '1' }, { url: '2' }] }]
+                    const newFindings = [{ word: 'a', refs: [{ url: '1' }] }]
 
                     const merged = mergeFindings(newFindings, existingFindings)
 
-                    assert.deepEqual(merged, [{ word: 'aa', refs: ['2'] }, { word: 'a', refs: ['1'] }])
+                    assert.deepEqual(merged, [{ word: 'aa', refs: [{ url: '2' }] }, { word: 'a', refs: [{ url: '1' }] }])
                 })
             })
         })
         describe('for existing with single ref', () => {
             describe('different word', () => {
                 it('should return existing (with removed unfound ref => empty refs) plus new', () => {
-                    const existingFindings = [{ word: 'aa', refs: ['1'] }]
-                    const newFindings = [{ word: 'a', refs: ['1'] }]
+                    const existingFindings = [{ word: 'aa', refs: [{url: '1' }] }]
+                    const newFindings = [{ word: 'a', refs: [{url: '1' }] }]
 
                     const merged = mergeFindings(newFindings, existingFindings)
 
-                    assert.deepEqual(merged, [{ word: 'aa', refs: [] }, { word: 'a', refs: ['1'] }])
+                    assert.deepEqual(merged, [{ word: 'aa', refs: [] }, { word: 'a', refs: [{url: '1' }] }])
                 })
             })
         })
